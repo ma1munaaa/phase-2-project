34db0165215b48a5db44760a399ab29dcbad0540
maimuna <mohamudmaimuna003@gmail.com>
2023-08-03 21:56:18 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Movie App
+
+A React application that fetches movie data from an external API and allows users to search, add favourite movies, and remove favourite movies.
+
+## About
+
+The Movie App is a simple React-based web application that fetches Movie  data from an external API. Users can view the list of movies, add favourite movies,  and remove favourite movies as needed.
+
+
+## featues
+
+- Fetches movie data from an external API.
+- Allows users to search movies.
+- Provides a filter option to add favourite movies.
+- Allows users to remove favourite movies.
+
+
+## Technologies used
+
+-React
+-Bootstrap
+-OMDB API
+
+## Installation
+
+1. Clone the repository to your local machine.
+
+2. Navigate to the project directory in the terminal.
+
+3. Install the required dependencies:
+
+```bash
+npm install
+```
+
+## Usage
+
+1. Start the development server:
+
+```bash
+npm start
+```
+
+2. Open your web browser and go to `http://localhost:3000` to access the movie App.
+
+3. The app will fetch movie data from the API and display it on the page. A loader will be shown while the data is loading.
+
+4. To add fovourite movies click on the below full heart
+
+6. To remove favourite movie, click on the "X" button 
+
+## License
+
+The Transactions App is open-source and released under the [MIT License](LICENSE).
+
+## Author
+
+Maimuna Mohamud
---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import axios from 'axios';
+import {Routes,Route,Link} from 'react-router-dom'
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavourites from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
+
+
+	function App() {
+	const [movies, setMovies] = useState([]);
+	const [favourites, setFavourites] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=73d92b96`;
+
+		try {
+			const response = await axios.get(url);
+			if (response.data.Search) {
+				setMovies(response.data.Search);
+			}
+		} catch (error) {
+			console.error('Error fetching movies:', error);
+		}
+	};
+
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
+	const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourites, movie];
+		setFavourites(newFavouriteList);
+	};
+
+	const removeFavouriteMovie = (movie) => {
+		const newFavouriteList = favourites.filter(
+			(favourite) => favourite.imdbID !== movie.imdbID
+		);
+		setFavourites(newFavouriteList);
+	};
+
+	return (
+		<div className='container-fluid movie-app'>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<Link to="/movies">Movies</Link>
+				<Link to="/favourites">Favourites</Link>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+				<Routes>
+		
+					<Route path='/' element={<MovieList movies={movies} handleFavouritesClick={addFavouriteMovie} favouriteComponent={AddFavourites} />} />
+					<Route path='/movies' element={<MovieList movies={movies} handleFavouritesClick={addFavouriteMovie} favouriteComponent={AddFavourites} />} />
+					<Route path='/favourites' element={<MovieList movies={favourites} handleFavouritesClick={removeFavouriteMovie} favouriteComponent={RemoveFavourites} />} />
+				</Routes>
+			</div>
+		</div>
+	);
+}
+		
+		export default App;
